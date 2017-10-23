224d4e5e25c807541b5213866b9e9d23bae8a834
fingerstlye94 <tal1994@gmail.com>
2017-10-23 16:27:35 +0300

    Http module improved - now accepting 'data' with relevant request parameters

--- js/utils/Http.js
+++ js/utils/Http.js
@@ -1,17 +1,24 @@
 (function(global) {
 
     var Http = {
-        get: function(url, params, callback) {
-            if(typeof params === 'object') {
+        get: function(url, data, callback) {
+            var xhr = new XMLHttpRequest();
+
+            if(typeof data.headers === 'object') {
+                for(var header in data.headers) if(data.headers.hasOwnProperty(header)) {
+                    xhr.setRequestHeader(header, data.headers[header]);
+                }
+            }
+            if(typeof data.params === 'object') {
                 var urlParams = '?';
-                for(var param in params) if(params.hasOwnProperty(param)) {
-                    urlParams += param + '=' + params[param] + '&';
+                for(var param in data.params) if(data.params.hasOwnProperty(param)) {
+                    urlParams += param + '=' + data.params[param] + '&';
                 }
                 url += urlParams;
             }
 
-            var xhr = new XMLHttpRequest();
             xhr.open('GET', url, true);
+
             xhr.onreadystatechange = function() {
                 if(xhr.status === 200 && xhr.readyState === 4) {
                     callback(JSON.parse(xhr.responseText));
@@ -25,8 +32,9 @@
 
             // analyse request data
             if(typeof data.headers === 'object') {
-                for(var header in data.headers)
+                for(var header in data.headers) if(data.headers.hasOwnProperty(header)) {
                     xhr.setRequestHeader(header, data.headers[header]);
+                }
             }
 
             if (typeof data.body === 'object') {
