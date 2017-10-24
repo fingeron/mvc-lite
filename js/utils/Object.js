78c7c43df37ae1e1bc9d261492614e42c1246c60
fingerstlye94 <tal1994@gmail.com>
2017-10-24 12:09:08 +0300

    added 'ignore' ability to 'updateObject' function

--- js/utils/Object.js
+++ js/utils/Object.js
@@ -1,14 +1,13 @@
 (function(global) {
 
     var ObjectFuncs = {
-        updateObject: function(origin, update) {
+        updateObject: function(origin, update, ignore) {
             var changes = 0;
             if(typeof origin === 'object' && typeof update === 'object') {
-                for(var property in update) if(update.hasOwnProperty(property)) {
+                for(var property in update) if(update.hasOwnProperty(property) && !isIgnored(property)) {
                     switch(typeof update[property]) {
                         case 'object':
-                            if(!Array.isArray(update[property]))
-                                changes += ObjectFuncs.updateObject(origin[property], update[property]);
+                            changes += ObjectFuncs.updateObject(origin[property], update[property]);
                             break;
                         default:
                             if(origin[property] !== update[property]) {
@@ -19,6 +18,20 @@
                     }
                 }
             }
+
+            function isIgnored(property) {
+                if(Array.isArray(ignore)) {
+                    for(var i = 0; i < ignore.length; i++) {
+                        if(property === ignore[i]) {
+                            return true;
+                        }
+                    }
+                } else if(property === ignore)
+                    return true;
+
+                return false;
+            }
+
             return changes;
         }
     };
